7b6692d212dd5f73d323f2839ead55576b6af4ff
Samatron <smadderom62@gmail.com>
2021-01-12 20:58:53 -0700

    1/12/2021 commit (added test driver teleop code for testing purposes)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team2844/TestDrivers/TestDriverMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team2844/TestDrivers/TestDriverMode.java
@@ -17,13 +17,17 @@ public class TestDriverMode extends LinearOpMode
          System.out.println("ValleyX: In Innit");
          waitForStart();
 
-         while (opModeIsActive()) {
+         while (opModeIsActive())
+         {
              double left;
              double right;
              double rtrigger;
              double ltrigger;
 
+             double currentSpeed = robot.backshot.getPower();
+
              // Gamepad 1
+             // driving
              left = -gamepad1.left_stick_y;
              right = -gamepad1.right_stick_y;
 
@@ -32,28 +36,67 @@ public class TestDriverMode extends LinearOpMode
              robot.rightFrontDrive.setPower(right);
              robot.rightBackDrive.setPower(right);
 
+             // intake
              rtrigger = gamepad1.right_trigger;
              ltrigger = gamepad1.left_trigger;
-
              // set intake motors to l and r triggers
 
+             // wobble goal arm
+             if (gamepad1.x)
+             {
+                 robot.wobbleServo.setPosition(0.1);
+             }
+             if (gamepad1.y)
+             {
+                 robot.wobbleServo.setPosition(0.0);
+             }
+             if (gamepad1.a)
+             {
+                 robot.clasper.setPosition(0.1);
+             }
+             if (gamepad1.b)
+             {
+                 robot.clasper.setPosition(0.0);
+             }
              telemetry.addData("LeftStickY = ",left);
              telemetry.addData("RightStickY = ", right);
              telemetry.update();
 
              // Gamepad 2
              // shooter (motors)
+             if (gamepad2.dpad_up)
+             {
+                 robot.backshot.setPower(currentSpeed+0.1);
+                 robot.frontshot.setPower(currentSpeed+0.1);
+             }
+             if (gamepad2.dpad_down)
+             {
+                 robot.backshot.setPower(currentSpeed-0.1);
+                 robot.frontshot.setPower(currentSpeed-0.1);
+             }
+             telemetry.addData("current speed: ", currentSpeed);
+             telemetry.update();
+
              // box (servo), presets for intake and loading
+             if (gamepad2.a)
+             {
+                 robot.nucketyServo.setPosition(0.1);
+             }
+             if (gamepad2.b)
+             {
+                 robot.nucketyServo.setPosition(0.0);
+             }
              // box ring pushing servo (incorporate into box preset buttons??)
-             // wobble goal arm (servo)
-
-
+             if (gamepad2.x)
+             {
+                 robot.sweepyServo.setPosition(0.1);
+             }
+             if (gamepad2.y)
+             {
+                 robot.sweepyServo.setPosition(0.0);
+             }
 
              // lights
          }
-
-
-
     }
-
 }
