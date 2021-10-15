a50377b49fa8fe15031bc27e3a85b886bfa71326
kadenmccarty <kadenmccarty@education.vchstrojans.org>
2021-10-15 16:25:56 -0700

    this is to make test programs

---
+++ Team2844/src/main/java/org/firstinspires/ftc/team2844/TestDrivers/TestDriver.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.team2844.TestDrivers;
+
+import org.firstinspires.ftc.team2844.Drivers.RobotHardware;
+
+public class TestDriver {
+    private RobotHardware robot_;
+    //test
+}
