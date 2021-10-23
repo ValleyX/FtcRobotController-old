22c1aaec1ff112279a687bc074ca12f372403202
drakeneiman <jairugar57@gmail.com>
2021-10-22 17:19:54 -0700

    Beginning of IMU

--- Team2844/src/main/java/org/firstinspires/ftc/team2844/TestDrivers/IMUDriverTest.java
+++ Team2844/src/main/java/org/firstinspires/ftc/team2844/TestDrivers/IMUDriverTest.java
@@ -1,5 +1,9 @@
 package org.firstinspires.ftc.team2844.TestDrivers;
 
+import org.firstinspires.ftc.team2844.Drivers.RobotHardware;
+
 public class IMUDriverTest {
+
+    private RobotHardware robot_;
 
 }
