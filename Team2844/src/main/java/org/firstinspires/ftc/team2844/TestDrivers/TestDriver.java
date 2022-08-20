aae81459b433db22f98afb29407f9cfd2025a4af
Jonathon Madderom <madderom@cox.net>
2022-08-20 15:50:41 -0700

    get ready for year 2022

--- Team2844/src/main/java/org/firstinspires/ftc/team2844/TestDrivers/TestDriver.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.team2844.TestDrivers;
-
-import org.firstinspires.ftc.team2844.Drivers.RobotHardware;
-
-public class TestDriver {
-    private RobotHardware robot_;
-    //test
-}
