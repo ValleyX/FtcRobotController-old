93379e6be715e48b05d64b2dffd7ceb6883dd344
Samatron <smadderom62@gmail.com>
2022-09-27 17:03:40 -0700

    we have jinnaly done it,  finnaly managed to make the camea work. it finds the alpha color based off of the camCam from bert. this is the code of the century and nobody will ever delete it or else mario will steal your liver. and laogi will go to bed with someone and you will have a brother in 9 months

---
+++ Team2844/src/main/java/org/firstinspires/ftc/team2844/cameaTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.team2844;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp (name="cameaTest")
+public class cameaTest extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RobotHardware robotHardware_ = new RobotHardware(this);
+
+
+
+        while(opModeInInit()){
+            //telemetry.addData("red value", robotHardware_.pipeline.avgLeftRed);
+            //telemetry.addData("blue value", robotHardware_.pipeline.avgLeftBlue);
+
+            telemetry.addData("r value", robotHardware_.pipeline.avgLeftR);
+            telemetry.addData("g value", robotHardware_.pipeline.avgLeftG);
+            telemetry.addData("b value", robotHardware_.pipeline.avgLeftB);
+
+            telemetry.addData("alpha color", robotHardware_.pipeline.color);
+
+            telemetry.update();
+        }
+        
+
+    }
+}
