6731933fc162acf63d44cbd39ab5e136c4af7f70
Josh Ettinger <Josh_Ettinger@yahoo.com>
2024-10-19 11:43:54 -0700

    Ben's Road runner Mecha legs build

---
+++ Team2844_Season2024_MechaLegs/src/main/java/org/firstinspires/ftc/teamcode/commands/CommandTest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.commands;
+import com.arcrobotics.ftclib.command.CommandBase;
+public class CommandTest extends CommandBase {
+    public CommandTest(){
+
+    }
+
+    @Override
+    public void initialize(){
+
+    }
+
+    @Override
+    public void execute(){
+
+    }
+
+    @Override
+    public boolean isFinished(){
+        return true;
+    }
+}
---
+++ Team2844_Season2024_MechaLegs/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SubsystemTest.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+public class SubsystemTest extends SubsystemBase {
+    public SubsystemTest(){
+
+    }
+
+    @Override
+    public void periodic(){
+
+    }
+}
